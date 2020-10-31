7a718e7475c4f04d6f1280b7dbfaf591ac58fea7
99002550 <71481084+99002550@users.noreply.github.com>
2020-10-31 17:08:37 +0530

    Update expenspec.js

--- expense/spec/expenspec.js
+++ expense/spec/expenspec.js
@@ -1,19 +1,19 @@
 describe('To test the customer Details', () => {
     let cust = null;
     it('should check name is of valid format', () => {
-        const cust = new customer("shahna", "xyz@gmail.com" , "31-10-2020","1234567890","745");
+        const cust = new customer("shahna", "xyz@gmail.com" , "1-10-2020","1234567890","855");
         expect(cust.name).toMatch("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$");        
         
     });
  
     it('should check whether name has min 5 characters', () => {
-        const cust = new customer("ABCDE", "xyz@gmail.com" , "1-10-2020","1234567890","854");
+        const cust = new customer("ABCDE", "xyz@gmail.com" , "1-10-2020","1234567890","954");
         expect(cust.name.length).toBe(5);
         
     });
     it('should check for valid name',()=>
     {
-        const cust = new customer("ABCDE", "xyz@gmail.com" , "1-10-2020","1234567890","100");
+        const cust = new customer("ABCDE", "xyz@gmail.com" , "1-10-2020","1234567890","700");
         expect(cust.phone.length).toBe(10);
     })
     it('should check for valid Email',()=>
@@ -23,12 +23,12 @@ describe('To test the customer Details', () => {
     })
     
     it('should check bill amount is min 500', () => {
-        const cust = new customer("ABCDE", "xyz@gmail.com" , "1-12-2020","8281268966","600");
+        const cust = new customer("ABCDEF", "xyz@gmail.com" , "1-12-2020","1234567890","600");
        expect(cust.billAmount).toBeGreaterThan(500);
         
     });
     it('should check if bill date is not more than one month old', () => {
-        const cust = new customer("ABCDE", "xyzy@gmail.com" , "1-10-2020","8281268966","600");
+        const cust = new customer("ABCDE", "xyzy@gmail.com" , "1-10-2020","1234567890","600");
         let date = cust.billDate;
         let x = getDate(date);
         let y = (x-1)*60 * 60 * 1000*24*30;
