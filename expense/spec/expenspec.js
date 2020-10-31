c9b4d7e0ba207387c4f0d3206ca0eae7d5cf5e19
99002550 <71481084+99002550@users.noreply.github.com>
2020-10-31 16:50:31 +0530

    Update expenspec.js

--- expense/spec/expenspec.js
+++ expense/spec/expenspec.js
@@ -7,28 +7,28 @@ describe('To test the customer Details', () => {
     });
  
     it('should check whether name has min 5 characters', () => {
-        const cust = new customer("ABCDE", "xyz@gmail.com" , "31-10-2020","1234567890","854");
+        const cust = new customer("ABCDE", "xyz@gmail.com" , "1-10-2020","1234567890","854");
         expect(cust.name.length).toBe(5);
         
     });
     it('should check for valid name',()=>
     {
-        const cust = new customer("ABCDE", "xyz@gmail.com" , "31-10-2020","1234567890","100");
+        const cust = new customer("ABCDE", "xyz@gmail.com" , "1-10-2020","1234567890","100");
         expect(cust.phone.length).toBe(10);
     })
     it('should check for valid Email',()=>
     {
-        const cust = new customer("ABCDE", "xyz@gmail.com" , "31-10-2020","1234567890","600");
+        const cust = new customer("ABCDE", "xyz@gmail.com" , "1-10-2020","1234567890","600");
         expect(cust.email).toMatch("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$");
     })
     
     it('should check bill amount is min 500', () => {
-        const cust = new customer("ABCDE", "happy@gmail.com" , "10-12-2020","8281268966","600");
+        const cust = new customer("ABCDE", "xyz@gmail.com" , "1-12-2020","8281268966","600");
        expect(cust.billAmount).toBeGreaterThan(500);
         
     });
     it('should check if bill date is not more than one month old', () => {
-        const cust = new customer("ABCDE", "xyzy@gmail.com" , "15-10-2020","8281268966","600");
+        const cust = new customer("ABCDE", "xyzy@gmail.com" , "1-10-2020","8281268966","600");
         let date = cust.billDate;
         let x = getDate(date);
         let y = (x-1)*60 * 60 * 1000*24*30;
